724e2cb1966631674ab1b36b36e99a6d93c596fa
dachen11 <20464465+dachen11@users.noreply.github.com>
2018-02-08 22:20:52 -0500

    Update daniel2.java
    feb08

--- w2-feb10/daniel2.java
+++ w2-feb10/daniel2.java
@@ -1,1 +1,91 @@
 ####
+
+//EASY PROBLEMS
+/*int x = 100;
+if (x > 0) {
+  print ("Hello World!");
+}*/
+
+/*int age = 20;
+if (age > 10) {
+  print ("Hi!");
+}
+else {
+  print ("Goodbye!");
+}*/
+
+/*int passcode = 100;
+if (passcode == 100) {
+  print (passcode);
+}
+else {
+  print ("Go away");
+}*/
+
+/*int x = 29183;
+int y = 1956;
+print (x * y);
+*/
+//MEDIUM PROBLEMS
+/*void draw() {
+  if (mousePressed) {
+    ellipse(mouseX, mouseY, 20, 20);
+  }
+}
+
+
+int x = 0;
+while (x < 10000) {
+  print (x);
+  print (" ");
+  x = x + 1;
+}*/
+/*void printTwice(String input) {
+  print (input);
+  print (input);
+}
+*/
+void setup(){
+ //printTwice("Hello");
+ //drawBlueCircle();
+ size(500,500);
+}
+//void drawBlueCircle() {
+  //fill(0,0,255);
+  //ellipse(100,100,50,50);
+//}
+
+//void draw() {
+//  float x = random(0,255);
+//  float y = random(0,255);
+//  float z = random(0,255);
+//  fill(x,y,z);
+//  ellipse(mouseX, mouseY, 50,50);
+//}
+
+//HARD PROBLEMS
+/*int x = 0;
+while (x < 10000) {
+  print (x);
+  print (" ");
+  x = x + 2;
+}*/
+
+//void draw() {
+//  float x = random(0,255);
+//  float y = random(0,255);
+//  float z = random(0,255);
+//  if (keyPressed) {
+//    fill(x,y,z);
+//  }
+//  if (mousePressed) {
+//    ellipse(mouseX, mouseY, 50,50);
+//  }
+//}
+
+
+
+
+
+
+
